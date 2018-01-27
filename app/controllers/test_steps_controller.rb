8e7e12abb1a16a77af5bfb7c5f7ffb80f5933e5e
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-01-27 17:43:35 +0000

    Done.

--- app/controllers/test_steps_controller.rb
+++ app/controllers/test_steps_controller.rb
@@ -4,11 +4,16 @@ class TestStepsController < ApplicationController
   end
   
   def new
+    binding.pry
     @test_step = TestStep.new
+    if params[:test_case_id] != nil
+        @test_case_id = params[:test_case_id].to_i
+        @test_step.test_case_id = params[:test_case_id].to_i
+    end 
   end 
   
   def create
-    binding.pry
+    # binding.pry
     
     
     redirect_to test_case_path(@test_case)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,13 @@ class UsersController < ApplicationController
     
     
     def new
-    
+         @user = User.new
     end 
     
     def create
         # binding.pry
         @user = User.new(user_params)
+        # binding.pry
         if @user.save
             session[:user_id] = @user.id
             redirect_to user_path(@user)
