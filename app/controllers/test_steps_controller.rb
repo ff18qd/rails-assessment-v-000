44416eb966388a46ad5f2038311f02aa405cfcff
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-01-27 22:55:35 +0000

    fix the add new step from test case page

--- app/controllers/test_steps_controller.rb
+++ app/controllers/test_steps_controller.rb
@@ -15,7 +15,8 @@ class TestStepsController < ApplicationController
     @test_step = TestStep.new(description: params[:test_step][:description])
     if @test_step.save
       @test_case_id = params[:test_case_id].to_i
-      redirect_to test_case_test_steps_path(:test_case_id => @test_case_id, :test_step_id =>  @test_step.id)
+      TestCaseTestStep.create(test_case_id: @test_case_id, test_step_id: @test_step.id)
+      redirect_to test_case_path(@test_case_id)
     else
        redirect_to user_path(session[:user_id])
     end 
