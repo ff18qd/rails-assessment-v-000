eabe0ebe15eedbf81df4b647468bc36c6a8b0668
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-01-30 02:08:10 +0000

    add note column to tcts join table and show note in tc show page

--- app/controllers/test_case_test_steps_controller.rb
+++ app/controllers/test_case_test_steps_controller.rb
@@ -3,6 +3,9 @@ class TestCaseTestStepsController < ApplicationController
         # binding.pry
         # redirect_to test case show page
         @test_case_test_step = TestCaseTestStep.new(test_case_id: params[:test_case_id], test_step_id: params[:test_step_id])
+        if params[:note]
+             @test_case_test_step.note =  params[:note]
+        end
         if @test_case_test_step.save 
             redirect_to test_case_path(params[:test_case_id])
         else 
