382aa291000d59684da41478d98df6cc4a06a591
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-04-02 01:28:26 +0000

    add list to append user index page

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,7 +2,7 @@ function loadTestCases(userid) {
     $.get("/users/" + userid + "/test_cases", function(resp) {
         // console.log(resp);
         resp.forEach(function(element) {
-            $("div#testcases").append(element.title);
+            $("div#testcases").append(`<li>${element.title}</li>`);
         })
     })
 }
