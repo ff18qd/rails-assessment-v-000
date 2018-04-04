b8fe429836a709b8b8b74a4cde938523d4f39eb6
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-04-04 01:05:33 +0000

    rendering test steps of each test case from Next button

--- app/assets/javascripts/test_case.js
+++ app/assets/javascripts/test_case.js
@@ -3,12 +3,15 @@ $(function() {
     var nextId = parseInt($(".js-next").attr("data-id")) + 1;
     // console.log(nextId);
     $.getJSON("/test_cases/" + nextId + ".json", function(data) {
-      console.log(data);
-      $("div.next_tc").append(`<h2>Title: ${data.title}</h2>`);
-      // $(".authorName").text(data["author"]["name"]);
-      // $(".postTitle").text(data["title"]);
-      // $(".postBody").text(data["description"]);
-      // // re-set the id to current on the link
+      console.log(data.test_steps);
+      $("div.next_tc").append(`<h2>Title: ${data.title}</h2><div id=${data.id}></div>`);
+      if (data.test_steps) {
+        data.test_steps.forEach(function(ele) {
+          $(`div#${data.id}`).append(`<li>${ele.description}</li>`)
+          //console.log(ele.description);
+        })
+      }
+      // re-set the id to current on the link
       $(".js-next").attr("data-id", data["id"]);
     });
   });
