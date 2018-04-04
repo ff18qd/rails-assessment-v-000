529b7b52ced9016a390be7e7c07c3f7692509379
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-04-04 00:05:32 +0000

    add next test case button on show page

--- app/assets/javascripts/test_case.js
+++ app/assets/javascripts/test_case.js
@@ -1,14 +1,16 @@
 $(function() {
   $(".js-next").on("click", function() {
     var nextId = parseInt($(".js-next").attr("data-id")) + 1;
-    console.log(nextId);
-    // $.getJSON("/posts/" + nextId + "/post_data", function(data) {
-    //   $(".authorName").text(data["author"]["name"]);
-    //   $(".postTitle").text(data["title"]);
-    //   $(".postBody").text(data["description"]);
-    //   // re-set the id to current on the link
-    //   $(".js-next").attr("data-id", data["id"]);
-    // });
+    // console.log(nextId);
+    $.getJSON("/test_cases/" + nextId + ".json", function(data) {
+      console.log(data);
+      $("div.next_tc").append(`<h2>Title: ${data.title}</h2>`);
+      // $(".authorName").text(data["author"]["name"]);
+      // $(".postTitle").text(data["title"]);
+      // $(".postBody").text(data["description"]);
+      // // re-set the id to current on the link
+      $(".js-next").attr("data-id", data["id"]);
+    });
   });
   
 });
